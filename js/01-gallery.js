2b68f6c3875c2cce56320f856fb95bc79348fecc
DimaSobko <sobko.dima@gmail.com>
2023-02-10 16:20:00 +0200

    ДЗ-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsGalleryContainer = galleryMarKup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsGalleryContainer);
+galleryContainer.addEventListener('click', onGalleryContainer)
+
+
+
+function galleryMarKup (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    }).join('')
+}
+
+function onGalleryContainer (event) {
+    event.preventDefault();
+    if(event.target.nodeName !=='IMG') {
+        return 
+    } 
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+        onShow: () => {
+            document.addEventListener("keydown", onEscPress);
+        }
+    })
+
+instance.show();
+
+function onEscPress (event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+}
+
+}
+
